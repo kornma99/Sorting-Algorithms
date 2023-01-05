f9ad68e1c59491c792ff120f78be01950b56e4c5
kornma99 <rjsduq99@gmail.com>
2023-01-05 18:04:21 +0900

    버블커밋

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,3 +1,13 @@
-def bubblesort():
-    for i in range(len(li)):
+def bubbleSort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        print(arr)
         
+        for j in range(0, n - i - 1):
+    
+            if arr[j] > arr[j + 1]: 
+                arr[j], arr[j + 1] = arr[j + 1], arr[j] 
+
+ar=[10,5,4,7,8,2,3,9]
+bubbleSort(ar)
