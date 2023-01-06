2f82e68c9f88865ab77a1f8d54a158e9f5d396aa
kornma99 <rjsduq99@gmail.com>
2023-01-06 20:54:18 +0900

    내용 추가

--- selection_sort.py
+++ selection_sort.py
@@ -2,9 +2,12 @@ def selection_sort(arr):
     length = len(arr)
     
     for i in range(length):
+        # 나중에 비교를 위해 i(인덱스값)을 다른 변수(min)에 저장해둔다
         min = i
         print(arr)
+        # 해당하는 회전의 i에 해당하는 원소와 나머지를 비교하기 위해 아래와 같은 range를 설정해준다.
         for j in range(i + 1, length):
+            # arr[min]과 arr[j]를 비교해서 전자가 더 작을경우 min에 j값을 넣어준다
             if arr[min] > arr[j]:
                 min = j
         if min != i:
@@ -14,3 +17,10 @@ def selection_sort(arr):
 
 arr1=[6,3,2,1,4]
 selection_sort(arr1)
+
+# 결과값
+# [6, 3, 2, 1, 4]
+# [1, 3, 2, 6, 4]
+# [1, 2, 3, 6, 4]
+# [1, 2, 3, 6, 4]
+# [1, 2, 3, 4, 6]
