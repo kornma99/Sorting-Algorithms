ed809f35516fcfe5505169212fa290d11c53c81f
kornma99 <rjsduq99@gmail.com>
2023-01-06 23:37:42 +0900

    내용 추가

--- insertion_sort.py
+++ insertion_sort.py
@@ -3,12 +3,15 @@ def insertion_sort(arr):
     
     for i in range(length):
         print(arr)
+        #외부 반복문의 i에 해당하는 원소와 다음 원소를 비교하기 위해
+        #j의 범위를 아래와 같이 한다.
         
         for j in range(i+1, length):
+            #외부 반복문에 i에 해당하는 원소와 j를 비교하고, i가 더
+            #작다면 위치를 바꿔준다.
             if arr[i] > arr[j]:
                 arr[i], arr[j] = arr[j], arr[i]
         
-    
     return arr
 
 arr1=[5,9,2,4,7,1,10]
