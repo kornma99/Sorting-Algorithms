12beb69c68ddf589d6b5c77d8a1e3b36450d14e4
kornma99 <rjsduq99@gmail.com>
2023-01-08 17:58:47 +0900

    머지소트 완료

--- merge_sort.py
+++ merge_sort.py
@@ -3,29 +3,41 @@ def merge_sort(arr) :
     if len(arr) <= 1 :
         return arr
     
+    #리스트를 반으로 쪼개기
     middle = len(arr) // 2
+    left = arr[:middle]
+    right = arr[middle:]
     
-    lowar = arr[ :middle]
-    highar = arr[middle: ]
+    #반으로 계속 쪼갠다. 더 이상 쪼개지지 않을 경우 위에 if문에 의해 arr을 return한다.
+    left_ = merge_sort(left)
+    right_ = merge_sort(right)
     
-    lowar_ = merge_sort(lowar)
-    highar_ = merge_sort(highar)
-    
-    return merge(lowar_, highar_)
+    return merge(left_, right_)
 
 
-def merge(list1, list2) :
-    i,j = 0,0
+def merge(left, right) :
+    i, j = 0,0
     final_arr = []
-    print(final_arr)
     
-    while i < len(list1) and j < len(list2):
+    while i < len(left) and j < len(right):
 
-        if list1[i] < list2[i] :
-            final_arr.append(list1[i])
+        if left[i] < right[j] :
+            final_arr.append(left[i])
             i += 1
             
         else:
-            final_arr.append(list2[i])
+            final_arr.append(right[j])
             j += 1
-        
+    while i < len(left):
+        final_arr.append(left[i])
+        i += 1
+    while j < len(right):
+        final_arr.append(right[j])
+        j += 1
+    return final_arr
+
+arr1 = [8,10,7,9,2,4,3,1,18,17,14]
+print(merge_sort(arr1))
+
+# 결과값
+# [1, 2, 3, 4, 7, 8, 9, 10, 14, 17, 18]
